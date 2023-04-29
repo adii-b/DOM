9857dffaa53c600977e74e9e46185fee2c67d850
Aditya Bharadwaj <adityabharadwaj273@gmail.com>
2023-04-29 13:55:49 +0530

    Completed assignment-7

--- DOM P7/script.js
+++ DOM P7/script.js
@@ -1,6 +1,27 @@
 let btn = document.querySelector(".main__form-btn");
+btn.type = "reset";
 btn.disabled = false;
 let input = document.querySelector(".main__form-input");
 input.disabled = false;
 
-function click() {}
+// Removing elements
+
+function click() {
+  input.value = "Hello"; // Sets text inside input
+  btn.addEventListener("click", () => {
+    for (const iterator of aTag) {
+      iterator.style.display = "inline";
+    }
+    btn.disabled = true;
+    input.disabled = true;
+    input.value = "";
+  });
+}
+
+click();
+
+let aTag = document.querySelectorAll(".main__languages a:nth-child(2n)");
+aTag = Array.from(aTag);
+for (const iterator of aTag) {
+  iterator.style.display = "none";
+}
